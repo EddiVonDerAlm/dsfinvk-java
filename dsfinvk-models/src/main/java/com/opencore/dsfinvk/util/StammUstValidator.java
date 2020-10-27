595e57fb9e26c15ce8875ff117b40bfb777e6488
Malte Neumann <m.neumann@prisma-computer.de>
2020-10-27 17:13:10 +0100

    New reduced vat rates added

--- dsfinvk-models/src/main/java/com/opencore/dsfinvk/util/StammUstValidator.java
+++ dsfinvk-models/src/main/java/com/opencore/dsfinvk/util/StammUstValidator.java
@@ -1,8 +1,11 @@
 package com.opencore.dsfinvk.util;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
@@ -10,7 +13,7 @@ import com.opencore.dsfinvk.models.stammdaten.StammUst;
 
 public class StammUstValidator implements ConstraintValidator<ValidStammUst, StammUst> {
 
-  private static final Map<Long, BigDecimal> UST_SAETZE = new HashMap<>();
+  private static final Map<Long, List<BigDecimal>> UST_SAETZE = new HashMap<>();
 
   @Override
   public boolean isValid(StammUst obj, ConstraintValidatorContext context) {
@@ -18,17 +21,20 @@ public class StammUstValidator implements ConstraintValidator<ValidStammUst, Sta
       return true;
     }
 
-    return UST_SAETZE.get(obj.getUstSchluessel()).compareTo(obj.getUstSatz()) == 0;
+
+    return UST_SAETZE.get(obj.getUstSchluessel())
+            .stream()
+            .anyMatch(val -> val.compareTo(obj.getUstSatz()) == 0);
   }
 
   static {
-    UST_SAETZE.put(1L, new BigDecimal("19.00"));
-    UST_SAETZE.put(2L, new BigDecimal("7.00"));
-    UST_SAETZE.put(3L, new BigDecimal("10.70"));
-    UST_SAETZE.put(4L, new BigDecimal("5.50"));
-    UST_SAETZE.put(5L, new BigDecimal("0.00"));
-    UST_SAETZE.put(6L, new BigDecimal("0.00"));
-    UST_SAETZE.put(7L, new BigDecimal("0.00"));
+    UST_SAETZE.put(1L, Arrays.asList(new BigDecimal("19.00"), new BigDecimal("16.00")));
+    UST_SAETZE.put(2L, Arrays.asList(new BigDecimal("7.00"), new BigDecimal("5.00")));
+    UST_SAETZE.put(3L, Arrays.asList(new BigDecimal("10.70")));
+    UST_SAETZE.put(4L, Arrays.asList(new BigDecimal("5.50")));
+    UST_SAETZE.put(5L, Arrays.asList(new BigDecimal("0.00")));
+    UST_SAETZE.put(6L, Arrays.asList(new BigDecimal("0.00")));
+    UST_SAETZE.put(7L, Arrays.asList(new BigDecimal("0.00")));
   }
 
 }
